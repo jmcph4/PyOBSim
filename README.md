d729d9e8dd7fdf753ca5f8c1737286d11922c6cc
Jack McPherson <hetra@users.noreply.github.com>
2016-10-03 21:43:52 +1000

    Init repo and add Order class
    This commit is the first commit and, as such, initialises the Git
    repository.
    
    This commit also adds the `Order` class, which represents an order in
    the market.

---
+++ README.md
@@ -0,0 +1,4 @@
+# PyOBSim - the Python Order Book Simulator#
+---
+
+PyOBSim is a Python module facilitating market simulation by implementing an order book and other utilities. PyOBSim aims to make testing of trading algorithms clean and simple.
---
+++ pyobsim/order.py
@@ -0,0 +1,90 @@
+BID_STR = "BID"
+ASK_STR = "ASK"
+CENTS_PER_DOLLAR = 100
+
+class Order(object):
+    def __init__(self, oid, owner, ticker, otype, price, qty):
+        if int(oid) < 0:
+            raise ValueError()
+
+        if str(otype) != BID_STR and str(otype) != ASK_STR:
+            raise ValueError()
+
+        if int(price * CENTS_PER_DOLLAR) <= 0:
+            raise ValueError()
+
+        if int(qty) <= 0:
+            raise ValueError()
+        
+        self._oid = int(oid)
+        self._owner = str(owner)
+        self._ticker = str(ticker)
+        self._otype = str(otype)
+        self._price = int(price * CENTS_PER_DOLLAR)
+        self._qty = int(qty)
+
+    @property
+    def oid(self):
+        return self._oid
+
+    @oid.setter
+    def oid(self, oid):
+        self._oid = oid
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        self._owner = str(owner)
+
+    @property
+    def ticker(self):
+        return self._ticker
+
+    @ticker.setter
+    def ticker(self, ticker):
+        self._ticker = str(ticker)
+
+    @property
+    def otype(self):
+        return self._otype
+
+    @property
+    def price(self):
+        return round(float(self._price / CENTS_PER_DOLLAR), 2)
+
+    @price.setter
+    def price(self, price):
+        if int(price * CENTS_PER_DOLLAR) <= 0:
+            raise ValueError()
+
+        self._price = int(price * CENTS_PER_DOLLAR)
+
+    @property
+    def qty(self):
+        return self._qty
+
+    @qty.setter
+    def qty(self, qty):
+        if int(qty) <= 0:
+            raise ValueError()
+
+        self._qty = int(qty)
+
+    def __str__(self):
+        return "{0}: {1} for {2} @ ${3} by {4}".format(self.ticker, self.otype,
+                                                       self.qty, self.price,
+                                                       self.owner)
+
+    def __repr__(self):
+        s = "ID: " + str(self.oid) + "\n"
+        s += "Owner: " + self.owner + "\n"
+        s += "Ticker: " + self.ticker + "\n"
+        s += "Type: " + self.otype + "\n"
+        s += "Price: $" + str(self.price) + "\n"
+        s += "Quantity: " + str(self.qty) + "\n"
+
+        return s
+
