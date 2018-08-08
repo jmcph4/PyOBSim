7159fd0ad049d8017185a1df06f4db906b30833e
Jack McPherson <jmcph4@users.noreply.github.com>
2018-08-08 15:21:43 +1000

    Rename variables in `Order` class
    This commit renames various variables in the `Order` class and allows
    for more accurate type structure when handling these variables.

--- pyobsim/order.py
+++ pyobsim/order.py
@@ -1,11 +1,9 @@
 CENTS_PER_DOLLAR = 100
 
-class Order(object):
-    def __init__(self, oid, owner, ticker, otype, price, qty):
-        if int(oid) < 0:
-            raise ValueError()
 
-        if str(otype) != "BID" and str(otype) != "ASK":
+class Order(object):
+    def __init__(self, id, owner, ticker, type, price, qty):
+        if int(id) < 0:
             raise ValueError()
 
         if round(float(price), 2) <= 0:
@@ -14,20 +12,16 @@ class Order(object):
         if int(qty) <= 0:
             raise ValueError()
         
-        self.__oid = int(oid)
+        self.__id = id
         self.__owner = owner
         self.__ticker = str(ticker)
-        self.__otype = str(otype)
+        self.__type = type
         self.__price = round(float(price), 2)
         self.__qty = int(qty)
 
     @property
-    def oid(self):
-        return self.__oid
-
-    @oid.setter
-    def oid(self, oid):
-        self.__oid = oid
+    def id(self):
+        return self.__id
 
     @property
     def owner(self):
@@ -35,7 +29,7 @@ class Order(object):
 
     @owner.setter
     def owner(self, owner):
-        self.__owner = str(owner)
+        self.__owner = owner
 
     @property
     def ticker(self):
@@ -46,8 +40,8 @@ class Order(object):
         self.__ticker = str(ticker)
 
     @property
-    def otype(self):
-        return self.__otype
+    def type(self):
+        return self.__type
 
     @property
     def price(self):
@@ -72,15 +66,15 @@ class Order(object):
         self.__qty = int(qty)
 
     def __str__(self):
-        return "{0}: {1} for {2} @ ${3} by {4}".format(self.ticker, self.otype,
+        return "{0}: {1} for {2} @ ${3} by {4}".format(self.ticker, self.type,
                                                        self.qty, self.price,
                                                        self.owner)
 
     def __repr__(self):
-        s = "ID: " + str(self.oid) + "\n"
-        s += "Owner: " + self.owner + "\n"
+        s = "ID: " + str(self.id) + "\n"
+        s += "Owner: " + str(self.owner) + "\n"
         s += "Ticker: " + self.ticker + "\n"
-        s += "Type: " + self.otype + "\n"
+        s += "Type: " + str(self.type) + "\n"
         s += "Price: $" + str(self.price) + "\n"
         s += "Quantity: " + str(self.qty) + "\n"
 
