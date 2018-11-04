73f0d3b26777d44b2323187a71b223e5be9bbc5b
Jack McPherson <jmcph4@users.noreply.github.com>
2018-11-04 16:41:29 +1000

    Format the `pyobsim.errors` module
    This commit reformats the `errors` module in the `pyobsim` package to
    comply with PEP8.

--- pyobsim/errors.py
+++ pyobsim/errors.py
@@ -1,17 +1,22 @@
 class InsufficientFundsError(Exception):
     pass
 
+
 class PriceOutOfRangeError(Exception):
     pass
+
 
 class InsufficientVolumeError(Exception):
     pass
 
+
 class NoPriceError(Exception):
     pass
 
+
 class ParticipantAlreadyExistsError(Exception):
     pass
+
 
 class NoSuchParameterError(Exception):
     pass
